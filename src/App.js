45e727b879cee88d9620f859f850779b2234fcb9
Patrick <zhen.patrick@gmail.com>
2021-04-19 17:38:25 -0400

    added news articles

--- src/App.js
+++ src/App.js
@@ -5,41 +5,47 @@ import firebase from "./firebase";
 const alanKey = process.env.REACT_APP_ALAN_KEY;
 
 const App = () => {
-  const [superheroes, setSuperheroes] = useState([]);
+  // const [superheroes, setSuperheroes] = useState([]);
+  const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-      alanBtn({
-          key: alanKey,
-          onCommand: ({command}) => {
-              if(command === "testcommand"){
-                alert("hello")
-              }
-          }
-      })
-  }, [])
-
-  useEffect(() => {
-    const data = async () => {
-      const db = firebase.firestore();
-      const data = await db.collection("Superheroes").get()
-    //   console.log(data.docs.map(doc => doc.data()))
-      setSuperheroes(data.docs.map((doc) => doc.data()));
-    };
-    data()
+    alanBtn({
+      key: alanKey,
+      // onCommand: ({ command, heroes }) => {
+      //   if (command === "super") {
+      //     console.log(heroes);
+      //     setSuperheroes(heroes);
+      //   }
+      // },
+      onCommand: ({command, articles}) => {
+        if (command === "news") {
+          console.log(articles)
+          setArticles(articles)
+        }
+      }
+    });
   }, []);
 
-  return (
-    <div>
-      <h1>The Golden Age of Superheroes</h1>
-      {
-        <h1>
-          {superheroes.map((superhero) => {
-            return superhero.Home;
-          })}
-        </h1>
-      }
-    </div>
-  );
+  return <h1>test</h1>;
+
+  // useEffect(() => {
+  //   const data = async () => {
+  //     const db = firebase.firestore();
+  //     const data = await db.collection("Superheroes").get()
+  //   //   console.log(data.docs.map(doc => doc.data()))
+  //     setSuperheroes(data.docs.map((doc) => doc.data()));
+  //   };
+  //   data()
+  // }, []);
+
+  // return (
+  //   <div>
+  //     <h1>The Golden Age of Superheroes</h1>
+  //         {superheroes.map((superhero) => {
+  //           return <li>{superhero.Home}</li>
+  //         })}
+  //   </div>
+  // );
 };
 
 export default App;
