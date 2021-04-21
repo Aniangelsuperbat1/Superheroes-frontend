c63271c961e5a6df773040f3b5205f87cfc617b8
Patrick <zhen.patrick@gmail.com>
2021-04-20 20:41:28 -0400

    added Single Article component

--- src/components/Newslayout/Layout.js
+++ src/components/Newslayout/Layout.js
@@ -6,4 +6,20 @@ import useStyles from "./styles.js";
 const Layout = ({ articles }) => {
   const classes = useStyles()
 
+  return (
+    <Grow in>
+      <Grid
+        className={classes.container}
+        container
+        alignItems="stretch"
+        spacing={3}
+      >
+        {articles.map((article, i) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: "flex" }}>
+            {/* <SingleArticle i={i} article={article} /> */}
+          </Grid>
+        ))}
+      </Grid>
+    </Grow>
+  );
 }
